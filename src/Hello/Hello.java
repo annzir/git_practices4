6e3585b00f062d8dab1ab8b94c95bc7ad32007e3
annzir <formydreams.aa@gmail.com>
2022-04-24 15:27:00 +0200

    push to github

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -3,5 +3,7 @@ package Hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("print some thing");
+        System.out.println("just push buttom");
+        System.out.println("other changing");
     }
 }
