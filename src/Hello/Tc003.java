61d99f612a59be9a48dfb0c89c727cb7ee5b6a48
annzir <formydreams.aa@gmail.com>
2022-04-24 16:56:05 +0200

    changing git

--- src/Hello/Tc003.java
+++ src/Hello/Tc003.java
@@ -3,5 +3,6 @@ package Hello;
 public class Tc003 {
     public static void main(String[] args) {
         System.out.println("practice of compilat");
+        //this changes from local intellij
     }
 }
